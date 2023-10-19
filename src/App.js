9cc0d221e3589ffaaef401f17c278f3af08c16b0
chirayu2107 <144110269+chirayu2107@users.noreply.github.com>
2023-10-19 15:34:35 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
         <div className="body">
           <div className="about">
             <h3><u>Description about me:</u></h3>
-            <p>Hey! I am Shubham, 19 Years of age and currently pursuing my B.Tech Degree from VIT in IT. I have been since the beginning of my time here on earth, fascinated by these machines<br/>that we call computers, and from understanding what the internet is and how it works to now learning to make my own websites is a great journey so far but<br/>there is so much I still don't know which makes me excited...</p>
+            <p>Hey! I am Chirayu, 19 Years of age and currently pursuing my B.Tech Degree from VIT in CSE. I have been since the beginning of my time here on earth, fascinated by these machines<br/>that we call computers, and from understanding what the internet is and how it works to now learning to make my own websites is a great journey so far but<br/>there is so much I still don't know which makes me excited...</p>
           </div>
           <div className="skills">
             <h3><u>Skills:</u></h3>
